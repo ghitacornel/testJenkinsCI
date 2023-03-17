242ec1aec1d02b94fc5b8737ccf6474b02020a54
ghita <ghita.cornel@gmail.com>
2023-03-17 08:00:59 +0200

    better

---
+++ src/main/resources/db/migration/V1.1__add_initial_data.sql
@@ -0,0 +1,4 @@
+insert into person (id,name,pass) values(1,'ion','dummy pass');
+insert into person (id,name,pass) values(2,'gheorghe','another pass');
+insert into person (id,name,pass) values(3,'ana','ana pass');
+insert into person (id,name,pass) values(4,'maria','maria123');
